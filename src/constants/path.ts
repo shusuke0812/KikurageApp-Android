b44301e82bb9ffdfc980f4d6ac47b9deb969e021
shusuke0812 <shu.ota0812@gmail.com>
2022-10-06 09:19:23 +0900

    add: screen paths

---
+++ src/constants/path.ts
@@ -0,0 +1,19 @@
+// launch
+export const TOP = 'TOP';
+export const LOGIN = 'LOGIN';
+export const SIGN_UP = 'SIGN_UP';
+export const DEVICE_REGISTER = 'DEVICE_REGISTER';
+// home
+export const HOME = 'HOME';
+// content
+export const CULTIVATTION = 'CULTIVATTION';
+export const CULTIVATTION_DETAIL = 'CULTIVATTION_DETAIL';
+export const POST_CULTIVATION = 'POST_CULTIVATION';
+export const RECIPE = 'RECIPE';
+export const POST_RECIPE = 'POST_RECIPE';
+export const COMMUNICATION = 'COMMUNICATION';
+// side menu
+export const CALENDAR = 'CALENDAR';
+export const GRAPH = 'GRAPH';
+export const SETTING = 'SETTING';
+export const DICTIONARY = 'DICTIONARY';
