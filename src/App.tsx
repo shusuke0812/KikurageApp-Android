05e8e261c04c09ebb1ad65bbda22b2fbc4addfbf
shusuke0812 <shu.ota0812@gmail.com>
2022-10-07 23:38:59 +0900

    fix: App use UiContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import Index from './index';
+import * as UiContext from './contexts/ui';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 function App() {
-  return <Index />;
+  const [applicationState, setApplicationState] = React.useState(
+    UiContext.createApplicationInitialState(),
+  );
+  return (
+    <UiContext.Context.Provider value={{applicationState, setApplicationState}}>
+      <View style={styles.container}>
+        <Text>Kikurage</Text>
+      </View>
+    </UiContext.Context.Provider>
+  );
 }
 
 export default App;
