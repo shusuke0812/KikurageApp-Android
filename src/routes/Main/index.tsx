650a7aed71706def14f43b9f43036974e6b7cddc
shusuke0812 <shu.ota0812@gmail.com>
2022-10-08 10:24:42 +0900

    add: routing

---
+++ src/routes/Main/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {createStackNavigator, StackCardInterpolationProps} from '@react-navigation/stack';
+import {TOP, LOGIN, HOME, RECIPE} from '../../constants/path';
+import {Top, Login, Home, Recipe} from '../../components/pages';
+import * as UiContext from '../../contexts/ui';
+
+const Stack = createStackNavigator();
+const forFade = ({current}: StackCardInterpolationProps) => ({
+  cardStyle: {
+    opacity: current.progress,
+  },
+});
+
+function switchingAuthStatus(status: UiContext.Status) {
+  switch (status) {
+    case UiContext.Status.UN_AUTHORIZED:
+      return <Stack.Screen name={TOP} component={Top} />;
+    case UiContext.Status.AUTHORIZED:
+      return <Stack.Screen name={HOME} component={Home} />;
+    case UiContext.Status.FIRST_OPEN:
+    default:
+      return <Stack.Screen name={TOP} component={Top} />;
+  }
+}
+
+function AuthWithRoutes() {
+  const uiContext = React.useContext(UiContext.Context);
+  return (
+    <Stack.Navigator>
+      {uiContext.applicationState !== UiContext.Status.LOADING ? (
+        switchingAuthStatus(uiContext.applicationState)
+      ) : (
+        <Stack.Screen name={LOADING} component={Loading} />
+      )}
+    </Stack.Navigator>
+  );
+}
+
+export default AuthWithRoutes;
