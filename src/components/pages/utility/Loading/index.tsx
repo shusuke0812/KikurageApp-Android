fcb35cd9d86f2d4878e01f797910dd6c94bf04f6
shusuke0812 <shu.ota0812@gmail.com>
2022-10-10 18:24:21 +0900

    add: temp button in Loading

--- src/components/pages/utility/Loading/index.tsx
+++ src/components/pages/utility/Loading/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import * as UiContext from '../../../../contexts/ui';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,10 +10,26 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function Loading() {
+interface Props {
+  state: UiContext.Status;
+}
+
+function ChangeStateButton(props: Props) {
+  const {setApplicationState} = React.useContext(UiContext.Context);
+  const {state} = props;
+  return (
+    <TouchableOpacity onPress={() => setApplicationState(state)}>
+      <Text>change state to {state}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function () {
   return (
     <View style={styles.container}>
-      <Text>Loading</Text>
+      <ChangeStateButton state={UiContext.Status.AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.UN_AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.FIRST_OPEN} />
     </View>
   );
 }
