76f741b031f075df6cabe92b4f6997a27c5af3c5
shusuke0812 <shu.ota0812@gmail.com>
2022-10-11 23:34:35 +0900

    add: Primary/Secondary button components

---
+++ src/components/atoms/PrimaryButton.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {StyleSheet, Text, TextStyle, ViewStyle} from 'react-native';
+import {Button as PaperButton} from 'react-native-paper';
+import {COLOR} from '../../constants/theme';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 17,
+    fontWeight: '700',
+    color: COLOR.WHITE,
+  },
+});
+
+interface Props {
+  onPress: () => void;
+  style?: ViewStyle | ViewStyle[];
+  textStyle?: TextStyle | TextStyle[];
+  titleLabel: string;
+  backgroundColor?: string;
+  disabled?: boolean;
+  disabledColor?: string;
+  icon?: string;
+}
+
+export default function PrimaryButton(props: Props) {
+  const {
+    onPress,
+    style,
+    textStyle,
+    titleLabel,
+    backgroundColor = COLOR.PRIMARY,
+    disabled,
+    disabledColor = COLOR.DISABLED,
+    icon,
+  } = props;
+
+  return (
+    <PaperButton
+      mode="contained"
+      onPress={onPress}
+      style={style}
+      disabled={disabled}
+      contentStyle={{
+        backgroundColor: disabled ? disabledColor : backgroundColor,
+      }}
+      icon={icon}>
+      {titleLabel && <Text style={[styles.text, textStyle]}>{titleLabel}</Text>}
+    </PaperButton>
+  );
+}
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,4 +2,5 @@ export const COLOR = {
   BACKGROUND: '#f2f2f7',
   PRIMARY: '#dc4c5e',
   WHITE: '#fff',
+  DISABLED: '#9a9a9a',
 };
