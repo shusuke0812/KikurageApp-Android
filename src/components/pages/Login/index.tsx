4643037d65b2c6ab4bb8cfba6d8aa48278a6eb85
shusuke0812 <shu.ota0812@gmail.com>
2022-10-17 21:37:39 +0900

    add: email/password field in Login

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {StyleSheet, View, TouchableWithoutFeedback} from 'react-native';
+import {PrimaryButton, dismiss, TextField} from '../../atoms';
+import {Context, Status} from '../../../contexts/ui';
+import {useControlledComponent} from '../../../lib/hooks';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 16,
+  },
+  textField: {
+    marginVertical: 20,
+  },
+  button: {
+    marginTop: 20,
   },
 });
 
 export default function Login() {
+  const {setApplicationState} = React.useContext(Context);
+  const mailAddress = useControlledComponent('');
+  const password = useControlledComponent('');
+
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={dismiss}>
+      <View style={styles.container}>
+        <TextField
+          label="email"
+          value={mailAddress.value}
+          onChangeText={mailAddress.onChangeText}
+          style={styles.textField}
+          autoCompleteType="email"
+        />
+        <TextField
+          label="password"
+          value={password.value}
+          onChangeText={password.onChangeText}
+          style={styles.textField}
+          autoCompleteType="password"
+          secureTextEntry={true}
+        />
+        <PrimaryButton
+          onPress={() => setApplicationState(Status.AUTHORIZED)}
+          style={styles.button}
+          titleLabel="Login"
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
