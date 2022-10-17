22b9772ed4ddf9efa9459e3072413ff62a1010d3
shusuke0812 <shu.ota0812@gmail.com>
2022-10-17 09:24:50 +0900

    add: TextField in atoms index

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export {default as PrimaryButton} from './PrimaryButton';
 export {default as SecondaryButton} from './SecondaryButton';
 export {default as Logo} from './Logo';
+export {default as TextField, dismiss} from './TextField';
