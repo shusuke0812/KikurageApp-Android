1f546f16303f448367a6d810b1d267938e17ca90
shusuke0812 <shu.ota0812@gmail.com>
2022-10-17 23:29:42 +0900

    add: Communication and Cultivation page files

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -3,3 +3,5 @@ export {default as Login} from './Login';
 export {default as SignUp} from './SignUp';
 export {default as Home} from './Home';
 export {default as Recipe} from './Recipe';
+export {default as Cultivation} from './Cultivation';
+export {default as Communication} from './Communication';
