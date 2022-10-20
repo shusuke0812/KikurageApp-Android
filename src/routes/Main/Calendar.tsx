8bba8681edf9a05e9489526ef5dc2d6a0e6c39e0
shusuke0812 <shu.ota0812@gmail.com>
2022-10-20 23:22:20 +0900

    add: drawer in Home

---
+++ src/routes/Main/Calendar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {CALENDAR} from '../../constants/path';
+import {Calendar} from '../../components/pages/sidemenu';
+
+const Stack = createStackNavigator();
+function CalendarNavigator() {
+  return (
+    <Stack.Navigator initialRouteName={CALENDAR}>
+      <Stack.Screen name={CALENDAR} component={Calendar} />
+    </Stack.Navigator>
+  );
+}
+
+export default CalendarNavigator;
--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -3,25 +3,41 @@ import {
   createStackNavigator,
   StackCardInterpolationProps,
 } from '@react-navigation/stack';
-import {TOP, HOME, LOADING} from '../../constants/path';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {TOP, HOME, LOADING, CALENDAR} from '../../constants/path';
 import Top from './Top';
 import Home from './Home';
+import Calendar from './Calendar';
 import {Loading} from '../../components/pages/utility';
 import * as UiContext from '../../contexts/ui';
 
 const Stack = createStackNavigator();
+const HomeDrawer = createDrawerNavigator();
 const forFade = ({current}: StackCardInterpolationProps) => ({
   cardStyle: {
     opacity: current.progress,
   },
 });
 
+function HomeWithDrawer() {
+  return (
+    <HomeDrawer.Navigator
+      initialRouteName={HOME}
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <HomeDrawer.Screen name={HOME} component={Home} />
+      <HomeDrawer.Screen name={CALENDAR} component={Calendar} />
+    </HomeDrawer.Navigator>
+  );
+}
+
 function switchingAuthStatus(status: UiContext.Status) {
   switch (status) {
     case UiContext.Status.UN_AUTHORIZED:
       return <Stack.Screen name={TOP} component={Top} />;
     case UiContext.Status.AUTHORIZED:
-      return <Stack.Screen name={HOME} component={Home} />;
+      return <Stack.Screen name={HOME} component={HomeWithDrawer} />;
     case UiContext.Status.FIRST_OPEN:
     default:
       return <Stack.Screen name={TOP} component={Top} />;
