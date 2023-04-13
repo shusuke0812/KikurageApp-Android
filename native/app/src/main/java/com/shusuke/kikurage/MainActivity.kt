55e6d085744a8b39633b229eac8844a6082db7e3
shusuke0812 <shu.ota0812@gmail.com>
2023-04-14 00:29:20 +0900

    Add native app project

---
+++ native/app/src/main/java/com/shusuke/kikurage/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shusuke.kikurage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
