3be15a9e2db74a4abfe7b866274edd467a973286
shusuke0812 <shu.ota0812@gmail.com>
2023-06-24 20:41:37 +0900

    add: the method getting paired device list

--- native/app/src/main/java/com/shusuke/kikurage/bluetooth/KikurageBluetoothManager.kt
+++ native/app/src/main/java/com/shusuke/kikurage/bluetooth/KikurageBluetoothManager.kt
@@ -2,9 +2,13 @@ package com.shusuke.kikurage.bluetooth
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Context
 import android.content.pm.PackageManager
+import androidx.annotation.RequiresPermission
 import androidx.core.app.ActivityCompat
+import com.shusuke.kikurage.bluetooth.entity.PairedDevice
+import com.shusuke.kikurage.bluetooth.entity.PairedDeviceList
 
 class KikurageBluetoothManager(
     private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
@@ -14,5 +18,18 @@ class KikurageBluetoothManager(
     }
     fun hasPermission(context: Context): Boolean {
         return ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED
+    }
+    @RequiresPermission(value = "android.permission.BLUETOOTH_CONNECT")
+    fun getPairedDevices(): PairedDeviceList {
+        val pairedDevice = bluetoothAdapter?.bondedDevices
+        val pairedDeviceList = PairedDeviceList()
+
+        pairedDevice?.forEach { device ->
+            val deviceName = device.name
+            val deviceMacAddress = device.address
+            val pairedDevice = PairedDevice(name = deviceName, macAddress = deviceMacAddress)
+            pairedDeviceList.items.add(pairedDevice)
+        }
+        return pairedDeviceList
     }
 }
