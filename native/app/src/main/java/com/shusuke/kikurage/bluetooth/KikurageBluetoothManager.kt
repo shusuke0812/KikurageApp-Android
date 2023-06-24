8c4ce049fb6f533ae9180a56a554023f1905d73b
shusuke0812 <shu.ota0812@gmail.com>
2023-06-24 20:08:41 +0900

    add: permission check method:

--- native/app/src/main/java/com/shusuke/kikurage/bluetooth/KikurageBluetoothManager.kt
+++ native/app/src/main/java/com/shusuke/kikurage/bluetooth/KikurageBluetoothManager.kt
@@ -1,11 +1,18 @@
 package com.shusuke.kikurage.bluetooth
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 
 class KikurageBluetoothManager(
     private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 ) {
     fun isSupported(): Boolean {
         return bluetoothAdapter == null
+    }
+    fun hasPermission(context: Context): Boolean {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED
     }
 }
