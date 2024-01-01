2f5e465b1260f88cfefbe3f971f039afb85d9136
shusuke0812 <shu.ota0812@gmail.com>
2024-01-01 16:56:50 +0900

    add: toolbar

--- native/app/src/main/java/com/shusuke/kikurage/AppRootActivity.kt
+++ native/app/src/main/java/com/shusuke/kikurage/AppRootActivity.kt
@@ -1,11 +1,20 @@
 package com.shusuke.kikurage
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class AppRootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_root)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
--- native/app/src/main/java/com/shusuke/kikurage/screen/top/TopFragment.kt
+++ native/app/src/main/java/com/shusuke/kikurage/screen/top/TopFragment.kt
@@ -1,10 +1,15 @@
 package com.shusuke.kikurage.screen.top
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.shusuke.kikurage.R
 
 class TopFragment : Fragment() {
@@ -14,7 +19,15 @@ class TopFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Toolbar
+        val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
+        val toolbarTitleTextView = toolbar?.findViewById<TextView>(R.id.toolbar_title)
+        toolbarTitleTextView?.setText(R.string.fragment_top_title)
     }
 }
