e034d17cd69663f838b18a3ff0544622bf5e8538
shusuke0812 <shu.ota0812@gmail.com>
2024-01-02 16:30:43 +0900

    fix: login fragment connects to view model

--- native/app/src/main/java/com/shusuke/kikurage/screen/login/LoginFragment.kt
+++ native/app/src/main/java/com/shusuke/kikurage/screen/login/LoginFragment.kt
@@ -1,14 +1,21 @@
 package com.shusuke.kikurage.screen.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.shusuke.kikurage.AppRootActivity
 import com.shusuke.kikurage.R
+import com.shusuke.kikurage.databinding.FragmentLoginBinding
+import timber.log.Timber
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel: LoginViewModel = LoginViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +27,13 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Toolbar
+        binding = FragmentLoginBinding.bind(view)
         (activity as AppRootActivity).setupToolbarTitle(R.string.fragment_login_title)
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEdittext.text.toString()
+            val password = binding.passwordEdittext.text.toString()
+            viewModel.login(email = email, password = password)
+        }
     }
 }
