c0c3bef22e19fc674b8020571079d1e01ab7d3cc
shusuke0812 <shu.ota0812@gmail.com>
2024-01-02 17:36:32 +0900

    fix: Home screen title

--- native/app/src/main/java/com/shusuke/kikurage/screen/home/HomeFragment.kt
+++ native/app/src/main/java/com/shusuke/kikurage/screen/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.shusuke.kikurage.AppRootActivity
 import com.shusuke.kikurage.R
 
 class HomeFragment : Fragment() {
@@ -13,7 +14,12 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppRootActivity).setupToolbarTitle(R.string.fragment_home_title)
     }
 }
