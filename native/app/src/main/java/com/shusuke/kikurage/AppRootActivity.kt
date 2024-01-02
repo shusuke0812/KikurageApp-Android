c091ed4bc23f259eed720c8e7c62bfa2621a3b1c
shusuke0812 <shu.ota0812@gmail.com>
2024-01-02 15:07:54 +0900

    add: setup Timber

--- native/app/src/main/java/com/shusuke/kikurage/AppRootActivity.kt
+++ native/app/src/main/java/com/shusuke/kikurage/AppRootActivity.kt
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.BuildConfig
+import timber.log.Timber
 
 class AppRootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_root)
+
         setupToolbar()
+        setupTimber()
+    }
+
+    fun setupToolbarTitle(id: Int) {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val toolbarTitleTextView = toolbar.findViewById<TextView>(R.id.toolbar_title)
+        toolbarTitleTextView?.setText(id)
     }
 
     private fun setupToolbar() {
@@ -17,9 +27,9 @@ class AppRootActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
     }
 
-    fun setupToolbarTitle(id: Int) {
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val toolbarTitleTextView = toolbar.findViewById<TextView>(R.id.toolbar_title)
-        toolbarTitleTextView?.setText(id)
+    private fun setupTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
