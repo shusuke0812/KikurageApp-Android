a3afa6994505a4a9f77a334efb31ed850b64b03c
shusuke0812 <shu.ota0812@gmail.com>
2024-01-02 17:16:44 +0900

    fix: LoginRepository on coroutine

--- native/app/src/main/java/com/shusuke/kikurage/repository/LoginRepository.kt
+++ native/app/src/main/java/com/shusuke/kikurage/repository/LoginRepository.kt
@@ -3,11 +3,14 @@ package com.shusuke.kikurage.repository
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.shusuke.kikurage.entity.response.LoginUser
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 interface LoginRepositoryInterface {
     suspend fun login(email: String, password: String) : Result<LoginUser, Error>
@@ -18,8 +21,8 @@ class LoginRepository : LoginRepositoryInterface {
 
     override suspend fun login(email: String, password: String) : Result<LoginUser, Error> {
         return withContext(Dispatchers.IO) {
-            val res = _auth.signInWithEmailAndPassword(email, password)
-            return@withContext if (res.isSuccessful) {
+            val authResult = auth(email, password)
+            return@withContext if (authResult != null) {
                 val user = _auth.currentUser
                 user?.let {
                     Ok(LoginUser(uid = user.uid, isEmailVerified = user.isEmailVerified))
@@ -27,6 +30,15 @@ class LoginRepository : LoginRepositoryInterface {
             } else {
                 Err(Error())
             }
+        }
+    }
+
+    private suspend fun auth(email: String, password: String) : AuthResult? {
+        return try {
+            val result = _auth.signInWithEmailAndPassword(email, password).await()
+            result
+        } catch (e: Exception) {
+            null
         }
     }
 }
