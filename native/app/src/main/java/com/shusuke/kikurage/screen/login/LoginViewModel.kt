b1955c4a369dec8ab1ee04e200f71e408e6a5ba2
shusuke0812 <shu.ota0812@gmail.com>
2024-01-02 11:39:37 +0900

    fix: included LoginRepository in LoginViewModel

--- native/app/src/main/java/com/shusuke/kikurage/screen/login/LoginViewModel.kt
+++ native/app/src/main/java/com/shusuke/kikurage/screen/login/LoginViewModel.kt
@@ -1,13 +1,31 @@
 package com.shusuke.kikurage.screen.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.michaelbull.result.mapBoth
+import com.shusuke.kikurage.repository.LoginRepository
+import com.shusuke.kikurage.repository.LoginRepositoryInterface
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(
+    private val loginRepository: LoginRepositoryInterface = LoginRepository()
+) : ViewModel() {
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState: StateFlow<LoginUiState> = _uiState
-    fun login() {}
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            loginRepository.login(email, password)
+                .mapBoth(
+                    success = { Log.d("test", "success to login") },
+                    failure = { Log.d("test", "fail to login") }
+                )
+        }
+
+    }
 }
 
 data class LoginUiState(
