0cc7dd4169fab6a1c0862698150917f7e7c773c0
shusuke0812 <shu.ota0812@gmail.com>
2024-01-03 17:45:37 +0900

    fix: integrates usecase in LoginViewModel

--- native/app/src/main/java/com/shusuke/kikurage/screen/login/LoginViewModel.kt
+++ native/app/src/main/java/com/shusuke/kikurage/screen/login/LoginViewModel.kt
@@ -2,8 +2,10 @@ package com.shusuke.kikurage.screen.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.michaelbull.result.flatMap
 import com.github.michaelbull.result.mapBoth
 import com.shusuke.kikurage.repository.LoginRepositoryInterface
+import com.shusuke.kikurage.usecase.LoadKikurageStateWithUserUseCaseInterface
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,7 +15,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginRepository: LoginRepositoryInterface
+    private val loginRepository: LoginRepositoryInterface,
+    private val loadKikurageStateWithUserUseCase: LoadKikurageStateWithUserUseCaseInterface
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState: StateFlow<LoginUiState> = _uiState
@@ -24,6 +27,7 @@ class LoginViewModel @Inject constructor(
         }
         viewModelScope.launch {
             loginRepository.login(email, password)
+                .flatMap { loadKikurageStateWithUserUseCase(it.uid) }
                 .mapBoth(
                     success = { _uiState.update { it.copy(isLogin = true, error = null) } },
                     failure = { _uiState.update { it.copy(isLogin = false, error = LoginError.LOGIN_FAILURE) } }
