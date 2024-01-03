94754724a03249b78ab4c8f8fa0bca0e3f5708b3
shusuke0812 <shu.ota0812@gmail.com>
2024-01-03 11:58:27 +0900

    add: Constant object

---
+++ native/app/src/main/java/com/shusuke/kikurage/constant/Constants.kt
@@ -0,0 +1,11 @@
+package com.shusuke.kikurage.constant
+
+object Constants {
+    object FirestoreCollectionName {
+        const val STATES = "kikurageStates"
+        const val GRAPH = "graph"
+        const val USERS = "kikurageUsers"
+        const val CULTIVATIONS = "cultivations"
+        const val RECIPES = "recipes"
+    }
+}
