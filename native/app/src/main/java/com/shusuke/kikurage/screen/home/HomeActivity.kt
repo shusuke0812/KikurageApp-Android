afe05d0a4f4520906ccf398f101fcc8c0ce773b7
shusuke0812 <shu.ota0812@gmail.com>
2024-01-06 21:45:03 +0900

    fix: setup toolbar in HomeActivity

--- native/app/src/main/java/com/shusuke/kikurage/screen/home/HomeActivity.kt
+++ native/app/src/main/java/com/shusuke/kikurage/screen/home/HomeActivity.kt
@@ -2,11 +2,30 @@ package com.shusuke.kikurage.screen.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.shusuke.kikurage.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.home_navigation_host_fragment)
+        navHostFragment?.findNavController()?.let {
+            val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment), drawerLayout)
+            toolbar.setupWithNavController(it, appBarConfiguration)
+        }
     }
 }
