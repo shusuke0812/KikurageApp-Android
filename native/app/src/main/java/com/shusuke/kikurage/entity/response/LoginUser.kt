41393685aebab8ad98a67061fbddf191921554ac
shusuke0812 <shu.ota0812@gmail.com>
2024-01-07 03:16:03 +0900

    add: default values of LoginUser

--- native/app/src/main/java/com/shusuke/kikurage/entity/response/LoginUser.kt
+++ native/app/src/main/java/com/shusuke/kikurage/entity/response/LoginUser.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class LoginUser (
-    val uid: String,
-    val isEmailVerified: Boolean
+    val uid: String = "",
+    val isEmailVerified: Boolean = false
 )
