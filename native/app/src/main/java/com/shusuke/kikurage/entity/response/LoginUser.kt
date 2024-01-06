47ac4a709af05acca911d23275f05a22c5b982d1
shusuke0812 <shu.ota0812@gmail.com>
2024-01-07 02:41:18 +0900

    add: @Serializable in LoginUser

--- native/app/src/main/java/com/shusuke/kikurage/entity/response/LoginUser.kt
+++ native/app/src/main/java/com/shusuke/kikurage/entity/response/LoginUser.kt
@@ -1,5 +1,8 @@
 package com.shusuke.kikurage.entity.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginUser (
     val uid: String,
     val isEmailVerified: Boolean
