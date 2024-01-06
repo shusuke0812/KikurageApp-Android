546bbeaeea3cca47a78b53ea6d97a1870c992fd9
shusuke0812 <shu.ota0812@gmail.com>
2024-01-06 21:26:09 +0900

    add: HomeActivity

---
+++ native/app/src/main/java/com/shusuke/kikurage/screen/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.shusuke.kikurage.screen.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shusuke.kikurage.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
