c184d12e7777528d3bc72cd6508e0b1d0f21352f
shusuke0812 <shu.ota0812@gmail.com>
2024-01-08 16:55:03 +0900

    add: AppRootViewModel

--- native/app/src/main/java/com/shusuke/kikurage/app/AppRootActivity.kt
+++ native/app/src/main/java/com/shusuke/kikurage/app/AppRootActivity.kt
@@ -1,22 +1,36 @@
-package com.shusuke.kikurage
+package com.shusuke.kikurage.app
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.shusuke.kikurage.screen.home.HomeActivity
+import com.shusuke.kikurage.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class AppRootActivity : AppCompatActivity() {
+    private lateinit var  viewModel: AppRootViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_root)
 
+        viewModel = ViewModelProvider(this@AppRootActivity)[AppRootViewModel::class.java]
         setupToolbar()
+
+        lifecycleScope.launch {
+            viewModel.uiState.collect() { uiState ->
+                if (uiState.isLogin) showHomeScreen()
+            }
+        }
     }
 
     // Call this method on each fragments
@@ -29,9 +43,10 @@ class AppRootActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.auth_navigation_host_fragment)
-        navHostFragment?.findNavController()?.let {
+        val navController =navHostFragment?.findNavController()
+        navController?.let {
             val appBarConfiguration = AppBarConfiguration(it.graph)
-            toolbar.setupWithNavController(it, appBarConfiguration)
+            setupActionBarWithNavController(it, appBarConfiguration)
         }
     }
 
