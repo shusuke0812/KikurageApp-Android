72d34a834f95fcfa69ea430c798e2749b018c157
shusuke0812 <shu.ota0812@gmail.com>
2024-02-28 23:52:57 +0900

    fix: KikurageBluetoothUUID

--- native/app/src/main/java/com/shusuke/kikurage/utility/bluetooth/KikurageBluetoothUUID.kt
+++ native/app/src/main/java/com/shusuke/kikurage/utility/bluetooth/KikurageBluetoothUUID.kt
@@ -2,12 +2,17 @@ package com.shusuke.kikurage.utility.bluetooth
 
 import android.bluetooth.le.ScanFilter
 
-sealed interface KikurageBluetoothUUID {
-    val uuidString: String
-    val scanFilter: ScanFilter
+object KikurageBluetoothUUID {
+    object LocalName {
+        val m5Stack = "kikurage-device-m5-stack"
+    }
+    object Service {
+        val m5Stack = "65609901-b6ed-45cc-b8af-b4055a9b7666"
+    }
+    object Characteristic {
+        val writeStopWiFiScan = "65609902-b6ed-45cc-b8af-b4055a9b7666"
+        val writeWiFiSetting = "65609904-b6ed-45cc-b8af-b4055a9b7666"
+        val readWiFi = "65609903-b6ed-45cc-b8af-b4055a9b7666"
+        val readCompletion = "65609905-b6ed-45cc-b8af-b4055a9b7666"
+    }
 }
-
-data class BleServiceUUID(
-    override val scanFilter: ScanFilter,
-    override val uuidString: String
-) : KikurageBluetoothUUID
